b07be0acf93b0a90127c77b92d443ebbbd7a1401
Alex PONDART <alex.pondart@oclock.school>
2025-07-07 10:14:29 +0200

    Configuration de l'environnement PostgreSQL avec l'utilisateur blogapi et la base blogapi

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { sequelize } from './config/database.js';
+import { Post } from './models/Post.js';
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +14,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API Blog fonctionne 🚀");
 });
+
+// Synchronisation BDD
+sequelize.sync()
+  .then(() => console.log('✅ Base de données synchronisée'))
+  .catch((error) => console.error('❌ Erreur de synchronisation:', error));
 
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`);
