3af2deb287afbfcf538401238ba4bb250878999e
Alex PONDART <alex.pondart@oclock.school>
2025-07-07 13:46:09 +0200

    Remise au propre de src/routes/authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,14 @@
-import { Router } from 'express';
-import { register, login } from '../controllers/authController.js';
+// src/routes/authRoutes.js
+
+import { Router } from "express";
+import { register, login } from "../controllers/index.js";
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
+// Route pour l'inscription d'un nouvel utilisateur
+router.post("/register", register);
+
+// Route pour la connexion et l'obtention d'un token JWT
+router.post("/login", login);
 
 export default router;
